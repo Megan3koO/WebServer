1aae728caea0840e6949dd9d6ca554d901e7c919
Megan3koO <minhdeptrai4869@gmail.com>
2025-03-14 15:39:01 +0700

    Add files via upload
    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+const PORT = process.env.PORT || 80;
+
+//GET API
+app.use("/", require("./apis/home"))
+app.use("/", require("./apis/test"))
+app.use("/", require("./apis/backtrace"))
+
+app.listen()
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running,  and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
