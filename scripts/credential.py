9b5ad8957c0ef7ff1e6fba90fa5405d7f9028176
Minh Dinh Cong <minh.dinh@opswat.com>
2025-04-21 11:44:48 +0700

    update file path

--- scripts/credential.py
+++ scripts/credential.py
@@ -3,43 +3,53 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
-
+import os
 import base64
 
 class CredentialManager:
     ErrorCode = {"SUCCESS" : 0, "FAILED" : -1}
 
     def __init__(self):
-        pass
+        # Set up file path
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(script_dir)
+        self.cwd = os.getcwd()
 
     def Encrypt(self, message: str):
-        with open("public_key.pem", "rb") as f:
-            public_key = serialization.load_pem_public_key(f.read())
+        encrypt = ""
+        try:
+            with open(os.path.join(self.cwd, "public_key.pem"), "rb") as f:
+                public_key = serialization.load_pem_public_key(f.read())
 
-        #message = b"{s}".format(s=i_message)
-        encrypted = public_key.encrypt(
-                    message.encode(),
-                    padding.OAEP(
-                        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                        algorithm=hashes.SHA256(),
-                        label=None))
-        encrypted = base64.b64encode(encrypted).decode('utf-8')
-        return encrypted
-        
-    def Decrypt(self, message: str):
-        with open("private_key.pem", "rb") as f:
-            private_key = serialization.load_pem_private_key(f.read(), password=None)
-
-        encrypted_bytes = base64.b64decode(message)
-        decrypted = private_key.decrypt(
-                        encrypted_bytes,
+            #message = b"{s}".format(s=i_message)
+            encrypted = public_key.encrypt(
+                        message.encode(),
                         padding.OAEP(
                             mgf=padding.MGF1(algorithm=hashes.SHA256()),
                             algorithm=hashes.SHA256(),
-                            label=None,
-                        )
-        )
+                            label=None))
+            encrypted = base64.b64encode(encrypted).decode('utf-8')
+        except Exception as e:
+            print(f"Error when trying to encrypt: {e}")
+        return encrypted
+        
+    def Decrypt(self, message: str):
+        decrypted = b""
+        try:
+            with open(os.path.join(self.cwd, "private_key.pem"), "rb") as f:
+                private_key = serialization.load_pem_private_key(f.read(), password=None)
 
+            encrypted_bytes = base64.b64decode(message)
+            decrypted = private_key.decrypt(
+                            encrypted_bytes,
+                            padding.OAEP(
+                                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                algorithm=hashes.SHA256(),
+                                label=None,
+                            )
+            )
+        except Exception as e:
+            print(f"Error when trying to decrypt: {e}")
         return decrypted.decode()
 
     def _GenerateRSAKey(self):
@@ -53,7 +63,7 @@ class CredentialManager:
         public_key = private_key.public_key()
 
         # Save private key to PEM file
-        with open("private_key.pem", "wb") as f:
+        with open(os.path.join(self.cwd, "private_key.pem"), "wb") as f:
             f.write(private_key.private_bytes(
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PrivateFormat.PKCS8,
@@ -61,7 +71,7 @@ class CredentialManager:
             ))
 
         # Save public key to PEM file
-        with open("public_key.pem", "wb") as f:
+        with open(os.path.join(self.cwd, "public_key.pem"), "wb") as f:
             f.write(public_key.public_bytes(
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PublicFormat.SubjectPublicKeyInfo,
